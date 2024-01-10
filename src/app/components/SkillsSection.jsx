60462903456c98b346188fc17c78789d2bc25037
davidcocc <davidcoccorullo7@gmail.com>
2024-01-10 14:03:02 +0100

    big fix

--- src/app/components/SkillsSection.jsx
+++ src/app/components/SkillsSection.jsx
@@ -19,7 +19,7 @@ const skillsCategories = [
 const SkillsSection = () => {
   const [openSections, setOpenSections] = useState([]);
   const [iconState, setIconState] = useState(
-    skillsCategories.map(() => <FaChevronDown size={16} style={{ opacity: 0.5 }}/>)
+    skillsCategories.map(() => <FaChevronDown key={index} size={16} style={{ opacity: 0.5 }}/>)
   );
 
   const toggleSection = (index) => {
